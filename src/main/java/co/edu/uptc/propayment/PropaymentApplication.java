51152417f9f9dff456118db0f2335738886c4d24
William Gonzalez <ruyk1120@gmail.com>
2026-05-11 13:28:42 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/propayment/PropaymentApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.propayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropaymentApplication.class, args);
+	}
+
+}
